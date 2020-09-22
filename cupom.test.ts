0d52574a048632df0666ee43eaa9a34d3126f21e
Gabriel de Jesus de <gabriel.franca@dce.ufpb.br>
2020-09-22 18:35:07 +0000

    Funcionando.

--- cupom.test.ts
+++ cupom.test.ts
@@ -1,8 +1,7 @@
 const cupom = require('./cupom');
 
 test('Loja Completa', () => {
-  expect(cupom.imprime_dados_loja()).toBe(
-    `Arcos Dourados Com. de Alimentos LTDA
+  expect(cupom.imprime_dados_loja()).toBe(`Arcos Dourados Com. de Alimentos LTDA
 Av. Projetada Leste, 500 EUC F32/33/34
 Br. Sta Genebra - Campinas - SP
 CEP:13080-395 Tel (19) 3756-7408
@@ -65,21 +64,26 @@ test('Inscrição estadual vazia', () => {
 test('Exercício 2 - customizado', () => {
 
   // Defina seus próprios valores para as variáveis a seguir
-  cupom.dados.nome_loja = "";
-  cupom.dados.logradouro = "";
-  cupom.dados.numero = 0;
+	cupom.dados.nome_loja = "Tropical";
+  cupom.dados.logradouro = "Rua siqueira Campos";
+  cupom.dados.numero = 580;
   cupom.dados.complemento = "";
-  cupom.dados.bairro = "";
-  cupom.dados.municipio = "";
-  cupom.dados.estado = "";
-  cupom.dados.cep = "";
-  cupom.dados.telefone = "";
+  cupom.dados.bairro = "Centro";
+  cupom.dados.municipio = "Paulista";
+  cupom.dados.estado = "Pernambuco";
+  cupom.dados.cep = "53401-320";
+  cupom.dados.telefone = "(81) 3438-5714";
   cupom.dados.observacao = "";
-  cupom.dados.cnpj = "";
-  cupom.dados.inscricao_estadual = "";
+  cupom.dados.cnpj = "37.886.772/0001-82";
+  cupom.dados.inscricao_estadual = "4232303-79";
 
   //E atualize o texto esperado abaixo
-  expect(cupom.imprime_dados_loja()).toBe(
-    `
+  expect(cupom.imprime_dados_loja()).toBe(`Tropical
+Rua siqueira Campos, 580
+Centro - Paulista - Pernambuco
+CEP:53401-320 Tel (81) 3438-5714
+
+CNPJ: 37.886.772/0001-82
+IE: 4232303-79
 `);
 });
